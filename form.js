fc699012a866fc96c79d1750673e3aa20ecdca34
Lydia Doza <doza@pdx.edu>
2022-05-06 10:40:09 -0700

    Update form.js
    collect values from input fields and log them to console on click event of submit button

--- form.js
+++ form.js
@@ -2,7 +2,26 @@ document.getElementById("btnSubmit").addEventListener("click",handleSubmit);
 
 function handleSubmit(event) {
     var input_name = document.getElementById("name").value;
-    console.log(input_name);
-
+    var input_email = document.getElementById("email").value;
+    var input_feedback = document.getElementById("feedback").value;
+    if (input_feedback === ""){
+        input_feedback = "No feedback was provided.";
+    }
     
+    var input_newsletter = document.getElementById("newsletter").checked;
+    if (input_newsletter){
+        input_newsletter = "Yes, join newsletter."
+    } else {
+        input_newsletter = "No, thank you."
+    }
+
+    var strOutput = 
+        ` Form input received
+        Name: ${input_name}
+        Email: ${input_email}
+        Feedback: ${input_feedback}
+        Singup for Newsletter: ${input_newsletter}
+        `;
+    
+    console.log(strOutput);
 }
