d3740a9b71a98827c2ed22c87e3183d0d4a608d3
Lydia Doza <doza@pdx.edu>
2022-05-06 10:31:31 -0700

    log value in name field upon button click

---
+++ form.js
@@ -0,0 +1,8 @@
+document.getElementById("btnSubmit").addEventListener("click",handleSubmit);
+
+function handleSubmit(event) {
+    var input_name = document.getElementById("name").value;
+    console.log(input_name);
+
+    
+}
