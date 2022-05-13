207a33ab4e9ad19f97015daeac4b52be51ee5550
Lydia Doza <doza@pdx.edu>
2022-05-12 18:15:09 -0700

    add wordlist html tag
    display sorted array key value pairs with hyphen between them

--- loadtext.js
+++ loadtext.js
@@ -1,5 +1,6 @@
 let input = document.getElementById('inputfile');
 let output = document.getElementById('output');
+let wordlist = document.getElementById('wordlist');
 
 function previewFile(){
     let files = input.files;
@@ -20,22 +21,37 @@ function previewFile(){
 
 function printFile(str){
     console.log(str);
+    // remove punctuation and lowercase letters
     let noPunctuation = str.replace(/[.,\/#\'!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
     
     let myArray = noPunctuation.split(/\s+/);
     console.log(myArray);
-    let map = new Map();
-    for(var i = 0; i < myArray.length-3;i++){
+    let map = {};
+    // populate associated array with 3 words: count
+    for(var i = 0; i < myArray.length-2;i++){
         let hashindex = myArray[i] + " " + myArray[i+1] + " " + myArray[i+2];
-        let hashval = 1;
-        if(map.has(hashindex)){
-            hashval= map.get(hashindex) + 1;
+        if(map.hasOwnProperty(hashindex)){
+            map[hashindex]++;
         } else {
-            hashval = 1;
+            map[hashindex] = 1;
         }
-        map.set(hashindex, hashval);
     }
 
-    
-    console.log(map);
+    // convert object into an array of 2-element arrays with [(3 words), count]
+    const frequencyArray = Object.keys(map).map(key => [key, map[key]]);
+    // sort the array by descending order
+    frequencyArray.sort((a,b) => b[1] - a[1]);
+    console.log(frequencyArray);
+
+    // take only first 100 entries or sorted array
+    const hundredTop = frequencyArray.slice(0,100);
+    console.log(hundredTop);
+    var strWordList = "";
+    hundredTop.forEach((arrayPair) => {
+        var line = arrayPair[0] + " - " + arrayPair[1];
+        console.log(line);
+        strWordList = strWordList.concat("\n",`${arrayPair[0]} - ${arrayPair[1]}`);
+        });
+
+    wordlist.textContent = strWordList;
 };
